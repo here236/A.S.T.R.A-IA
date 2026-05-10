7fc7dc17ebc676a04c17d07f8df20857da834195
NAKO-0 <henrycosfer@gmail.com>
2026-05-09 21:13:57 -0300

    Tentaativa de capturar tela

--- captura_tela/capturador_tela.py
+++ captura_tela/capturador_tela.py
@@ -1,12 +1,29 @@
+import tkinter as tk
+import mss
+import mss.tools
+
 '''
 Responsável por tirar o “print”
 
-*Capturar tela inteira
-*Capturar regiões específicas
+* Capturar tela inteira
+* Capturar regiões específicas
 '''
 
 def capturar_tela():
-    pass
+    with mss.mss() as sct:
+        sct.shot(output="tela.png")
+
 
 def capturar_regiao(x, y, largura, altura):
-    pass
+    root = tk.Tk()
+
+    largura_tela = root.winfo_screenwidth()
+    altura_tela = root.winfo_screenheight()
+
+    print(f"{largura_tela}x{altura_tela}")
+
+    root.destroy()
+
+    return
+
+capturar_regiao(0, 0, 100, 100)
