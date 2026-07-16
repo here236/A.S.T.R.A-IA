933a0fc20fba58e5e4c53ae76fc622fb501f1efa
NAKO-0 <henrycosfer@gmail.com>
2026-07-16 16:32:03 -0300

    obtem o id da janela e sua localizaçao

--- captura_tela/gerenciador_janelas.py
+++ captura_tela/gerenciador_janelas.py
@@ -1,3 +1,4 @@
+import win32gui
 '''
 Controla qual parte da tela será capturada
 
@@ -5,9 +6,21 @@ Controla qual parte da tela será capturada
 Identificar janela ativa
 Obter posição e tamanho
 '''
-
+# receber o id na janela do windows
 def obter_janela_ativa():
-    pass
+     janela = win32gui.GetForegroundWindow()
+     return janela
 
+#localizar onde esta a janela ativa
 def obter_dimensoes_janela():
-    pass
+    handle = obter_janela_ativa()
+
+    left, top, right, bottom = win32gui.GetWindowRect(handle)
+
+    largura = right - left
+    altura = bottom - top
+
+    return left, top, largura, altura
+
+print("Handle:", obter_janela_ativa())
+print("Dimensões:", obter_dimensoes_janela())
